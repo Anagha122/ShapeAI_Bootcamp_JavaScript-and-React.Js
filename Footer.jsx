986c922d01d2f5e8593a40e2725332ba1c2aa0ca
Anagha122 <85427827+Anagha122@users.noreply.github.com>
2021-06-07 11:42:43 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+
+var currYear =new Date().getFullYear();
+
+  return (
+    <footer>
+    <p>Copyright @ 2021</p>
+<h1>shapeAI</h1>
+    </footer>
+  );
+}
+
+export default Footer;
